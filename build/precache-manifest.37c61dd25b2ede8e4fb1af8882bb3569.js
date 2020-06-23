9e6c3e09e63d10fb3f3985f9d0b0b543e47027ad
Roth <roth1002@gmail.com>
2020-06-23 20:58:44 +0800

    show build

---
+++ build/precache-manifest.37c61dd25b2ede8e4fb1af8882bb3569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f11aef0e74f9d8ac98fc1957811ab7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1053987c5126bbbbcb4d",
+    "url": "/static/css/main.f7f69cfc.chunk.css"
+  },
+  {
+    "revision": "5fffc4752eef7d8defdf",
+    "url": "/static/js/2.2c2264b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c2264b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1053987c5126bbbbcb4d",
+    "url": "/static/js/main.3696dcf6.chunk.js"
+  },
+  {
+    "revision": "06b0026a7edd90ee6c1d",
+    "url": "/static/js/runtime-main.cecf3bba.js"
+  }
+]);
